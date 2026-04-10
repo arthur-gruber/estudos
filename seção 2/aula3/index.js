8db2bb4fe401aabdc8fcc252b4749adf57960294
Arthur Costa Gruber <arthurcg21@gmail.com>
2026-04-10 11:02:22 -0300

    learning about variables

---
+++ seção 2/aula3/index.js
@@ -0,0 +1,16 @@
+let nome; // declarando variavel
+nome = 'Arthur'; // atribuindo valor a variavel
+console.log(nome); // exbindo variavel
+
+let idade = 23; // declarando e atribuindo valor a variavel
+console.log(idade); // exibindo variavel
+
+/*
+ nao criar variaveis com palavras reservadas
+ nao redeclarar variavel
+ criar variaveis com nomes significativos (ex: nomeCompleto, idadeUsuario, faceis de entender)
+ nao criar variaveis comecando com numero
+ nao usar espacos ou traços (ex: nome-completo, nome completo)
+ usar camelCase para variaveis com mais de uma palavra (ex: nomeCompleto, idadeUsuario)
+ Case-sensitive (ex: nomeCompleto e nomecompleto sao variaveis diferentes)
+ */
