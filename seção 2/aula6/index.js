e9b9c1ae78ac661bd272970643fcf6bbecfc20c8
Arthur Costa Gruber <arthurcg21@gmail.com>
2026-04-10 11:47:40 -0300

    diference between var and let

---
+++ seção 2/aula6/index.js
@@ -0,0 +1,11 @@
+// diference between var and let
+var nome = 'Arthur';
+console.log(nome);
+var nome = 'Maria';
+console.log(nome);
+
+let nome2 = 'Arthur';
+console.log(nome2);
+/*let nome2 = 'Maria';
+console.log(nome2); error, let não pode ser redeclarada
+*/
