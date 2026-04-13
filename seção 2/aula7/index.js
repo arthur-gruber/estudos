5a15c48fd884f35438f90e340a38e16b2f7866e6
Arthur Costa Gruber <arthurcg21@gmail.com>
2026-04-13 09:58:05 -0300

    Types of primative data

---
+++ seção 2/aula7/index.js
@@ -0,0 +1,21 @@
+// Types of primitive data (string, number, boolean, etc.)
+
+// String
+let name = "Arthur";
+let name2 = 'Costa';
+let name3 = `Gruber`;
+console.log(name + name2 + name3);
+
+// Number
+let number1 = 10;
+let number2 = 20.51;
+console.log(number1 + number2);
+
+// Undefined
+let nomeCompleto; // valor undefined, pois não foi atribuído nenhum valor
+
+// Null
+let idade = null; // valor null, pois foi atribuído o valor null
+
+// Boolean
+const aprovado = true; // verdadeiro ou falso (valor logico)
