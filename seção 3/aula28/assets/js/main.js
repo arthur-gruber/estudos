8f6bfe4157c68cb0a0c95887bb57f10ed5007e10
arthur <arthurcg21@gmail.com>
2026-04-27 10:28:48 -0300

    exercise

---
+++ seção 3/aula28/assets/js/main.js
@@ -0,0 +1,28 @@
+const elementos = [
+    {tag: 'p', texto: 'Frase 1'},
+    {tag: 'div', texto: 'Frase 2'},
+    {tag: 'footer', texto: 'Frase 3'},
+    {tag: 'section', texto: 'Frase 4'},
+];
+
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+
+for (let i = 0; i <elementos.length; i++){
+    let {tag, texto} = elementos[i];
+    let tagCriada = document.createElement(tag);
+    tagCriada.innerText = texto; // Usar innerText para evitar que o texto seja interpretado como HTML
+    div.appendChild(tagCriada);
+}
+
+container.appendChild(div);
+
+// eu (deu errado)
+// const container = document.querySelector('.container');
+
+// // for(let i = 0; i <elementos.length; i++){
+// //     let {tag, texto} = elementos[i];
+// //     let tagCriada = document.createElement(tag);
+// //     tagCriada.innerHTML = texto;
+// //     document.body.appendChild(tagCriada);
+// // }
