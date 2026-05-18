5188a8363b11e879abc05b9397b3194e768de5e4
arthur <arthurcg21@gmail.com>
2026-05-18 09:43:14 -0300

    exercise

---
+++ seção 3/aula34/script.js
@@ -0,0 +1,11 @@
+// escreva uma funcao que recebe dois numeros e retorna o maior deles
+
+// function maiorNumero(num1, num2) {
+//     // if (num1 > num2) return num1;
+//     // return num2;
+//     return num1 > num2 ? num1 : num2; 
+// }
+
+
+const max = (x, y) => x > y ? x : y; 
+console.log(max(10, 20)); 
