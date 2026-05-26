ec9236514db262935f9bf3b38a6e048297c0a3da
arthur <arthurcg21@gmail.com>
2026-05-26 09:43:52 -0300

    filter

---
+++ secao5/aula4/script.js
@@ -0,0 +1,7 @@
+// filter, map e reduce
+
+// retornar numeros maiores que 10 
+const n = [21, 32, 31, 44, 87, 1, 2, 5, 9, 92];
+
+const maiorQue10 = n.filter((num) => num > 10);
+console.log(maiorQue10);
