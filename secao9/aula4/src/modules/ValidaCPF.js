42ac51b369214f0caf57e1b2a3b69d8bde5b3cd6
arthur <arthurcg21@gmail.com>
2026-06-10 11:28:34 -0300

    created a generator of valid cpf

---
+++ secao9/aula4/src/modules/ValidaCPF.js
@@ -0,0 +1,46 @@
+// 872.804.630-70 -> cpf gerado
+export default class ValidaCPF {
+    constructor(cpfEnviado) {
+        Object.defineProperty(this, 'cpfLimpo', {
+            writable: false,
+            enumerable: true,
+            configurable: false,
+            value: cpfEnviado.replace(/\D+/g, '')
+        })
+    }
+
+    isSequencia() {
+        return this.cpfLimpo.charAt(0).repeat(this.cpfLimpo.length) === this.cpfLimpo;
+    }
+
+    geraNovoCpf() {
+        const cpfParcial = this.cpfLimpo.slice(0, -2);
+        const digito1 = ValidaCPF.geraDigito(cpfParcial);
+        const digito2 = ValidaCPF.geraDigito(cpfParcial + digito1);
+        this.novoCPF = cpfParcial + digito1 + digito2;
+    }
+    
+    static geraDigito(cpfParcial) { 
+        let total = 0;
+        let regressivo = cpfParcial.length + 1;
+
+        for (let stringNumerica of cpfParcial) {
+            total += regressivo * Number(stringNumerica);
+            regressivo--;
+        }
+
+        const digito = 11 - (total % 11);
+        return digito > 9 ? '0' : String(digito);
+        
+    }
+
+
+    valida() {
+        if (!this.cpfLimpo) return false;
+        if (this.cpfLimpo.length !== 11) return false;
+        if (typeof this.cpfLimpo !== 'string') return false;
+        if (this.isSequencia()) return false;
+        this.geraNovoCpf();
+        return this.novoCPF === this.cpfLimpo;
+    }
+}
