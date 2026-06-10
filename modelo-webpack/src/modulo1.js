3a7b1bfaba417db9e9406e3534245882d192ccb1
arthur <arthurcg21@gmail.com>
2026-06-10 10:42:28 -0300

    created modelo-webpack

---
+++ modelo-webpack/src/modulo1.js
@@ -0,0 +1,10 @@
+const nome = 'Arthur';
+const sobrenome = 'costa';
+const idade = 24;
+export const salario = 2000;
+
+function soma(x, y) {
+    return x + y;
+}
+
+export { nome, sobrenome, idade, soma };
