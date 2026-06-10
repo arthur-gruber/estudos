1e6686ebc469fd1af5c5a4c5f25fbae5bee920c0
arthur <arthurcg21@gmail.com>
2026-06-10 10:32:16 -0300

    import & export ES6 Modules

---
+++ secao9/aula3/public/assets/js/bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log("-----","Arthur",2e3),console.log(5)})();
+//# sourceMappingURL=bundle.js.map
---
+++ secao9/aula3/src/index.js
@@ -0,0 +1,5 @@
+import { nome as nome2, sobrenome, idade, soma, salario } from './modulo1.js';
+
+const nome = '-----';
+console.log(nome, nome2, salario);
+console.log(soma(2, 3));
