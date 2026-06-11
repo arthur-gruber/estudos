9394f4e4bad6aaa8bba81362378a75a26ab5f315
arthur <arthurcg21@gmail.com>
2026-06-11 09:52:51 -0300

    writing and reading files

---
+++ secao10/aula3/app.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const caminhoArquivo = path.resolve(__dirname, 'teste.json');
+const escreve = require('./modules/escrever');
+const ler = require('./modules/ler');
+
+const pessoas = [
+    { nome: 'João' }, 
+    { nome: 'Maria' }, 
+    { nome: 'Pedro' }, 
+    { nome: 'Ana' }
+];
+const json = JSON.stringify(pessoas, '', 2);
+escreve(caminhoArquivo, json);
+
+async function lerArquivo(caminho) {
+    const dados = await ler(caminho);
+    renderizaDados(dados);
+}
+
+function renderizaDados(dados) {
+    dados = JSON.parse(dados);
+    dados.forEach(val => { console.log(val.nome); });
+}
+
+lerArquivo(caminhoArquivo);
